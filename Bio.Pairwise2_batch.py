17c0d8cebfcf67647a383876181036f7049ef7be
Feng JU <RichieJu520@users.noreply.github.com>
2017-12-20 11:42:37 +0100

    Update Bio.Pairwise2_batch.py

--- Bio.Pairwise2_batch.py
+++ Bio.Pairwise2_batch.py
@@ -1,4 +1,9 @@
-# Bio.Pairwise2_batch.py
+# -*- coding: utf-8 -*-
+"""
+@author: Feng Ju
+@email: richieju520@gmail.com
+The script was written and tested in python 2.7 and biopython 1.58
+"""
 
 from Bio import pairwise2
 from Bio import SeqIO
